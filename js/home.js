04bcf07d91fced9db6b4856f8bed810154ddc42a
dong hoon kim <donghoonkim1226@gmail.com>
2016-01-14 15:28:35 -0500

    Intro to JS - Finished exercise

---
+++ js/home.js
@@ -0,0 +1,6 @@
+function myFunction() {
+	var myName = prompt ("what's your name?");
+	var myColor = prompt ("what's your favorite color??");
+	var mySentence = myName + " likes " + myColor;
+	console.log(mySentence);
+}
